670895f7be07ab16b80119fa48fdb19ff0316b78
André Tashiro <andre_seichi@hotmail.com>
2021-08-08 16:08:26 -0300

    Quote component

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -2,28 +2,10 @@ import PropTypes from 'prop-types';
 
 export const Quote = ({ anime, character, quote }) => {
   return (
-    // <div>
-    //   <h2>Anime: {anime}</h2>
-    //   <p>Character: {character}</p>
-    //   <p>{quote}</p>
-    // </div>
-
-    <div>
-      <pre>
-        {'{'}
-        {'\n'}
-        {'"anime":'} {'"'}
-        {anime}
-        {'"'},{'\n'}
-        {'"character":'} {'"'}
-        {character}
-        {'"'},{'\n'}
-        {'"quote":'} {'"'}
-        {quote}
-        {'"'}
-        {'\n'}
-        {'}'},
-      </pre>
+    <div className="quote">
+      <h2>Anime: {anime}</h2>
+      <p>Character: {character}</p>
+      <p>{quote}</p>
     </div>
   );
 };
